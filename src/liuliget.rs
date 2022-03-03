d18e54cfe7ea0422c41303555a19a7a018614a79
alan <alan@fanruan.com>
2022-03-03 14:19:29 +0800

    update

--- src/liuliget.rs
+++ src/liuliget.rs
@@ -1,14 +1,40 @@
+use nipper::Document;
+
 #[derive(Debug)]
 pub struct Liuliget {
-    working: bool,
+}
+
+#[derive(Debug)]
+pub struct Post {
+    pub title: String,
+    pub url: String,
+    pub description: String,
+    pub img: String,
+    pub post_type: String,
 }
 
 impl Liuliget {
-    pub fn new() -> Liuliget {
-        Liuliget { working: false }
-    }
-
-    pub fn start(&mut self) {
-        self.working = true;
+    pub async fn get_page() -> Result<Vec<Post>, Box<dyn std::error::Error>> {
+        let res = reqwest::get("https://www.hacg.cat/wp/").await?.text().await?;
+        let document = Document::from(res.as_str());
+        let mut posts: Vec<Post> = Vec::new();
+        document.select(".status-publish").iter().for_each(|athing| {
+            let title = athing.select(".entry-title a").text();
+            if !title.is_empty(){
+                let url = athing.select(".more-link").attr("href").unwrap_or_default();
+                let description = athing.select(".entry-content p").text().replace(" 继续阅读 →", "");
+                let img = athing.select(".entry-content img").attr("src").unwrap_or_default();
+                let post_type = athing.select(".cat-links").text().replace("发表在 ", "");
+                let post = Post {
+                    title: title.to_string(),
+                    url: url.to_string(),
+                    description: description,
+                    img: img.to_string(),
+                    post_type: post_type.to_string(),
+                };
+                posts.push(post);
+            }
+        });
+        Ok(posts)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,46 @@
-use teloxide::{prelude::*, utils::command::BotCommand};
+use teloxide::{prelude2::*, utils::command::BotCommand};
+use teloxide::types::InputFile;
 use std::error::Error;
+use reqwest::Url;
 mod liuliget;
 
-#[derive(BotCommand, Debug, PartialEq)]
-#[command(rename = "lowercase", description = "支持的命令清单有:")]
+#[derive(BotCommand, Clone)]
+#[command(rename = "lowercase", description = "These commands are supported:")]
 enum Command {
     #[command(description = "查看帮助")]
     Help,
-    #[command(description = "开启定时监测")]
+    #[command(description = "开始使用")]
     Start,
-    #[command(description = "停止定时监测")]
-    Stop,
-    #[command(description = "获取第一页的内容")]
-    Refresh,
-    #[command(description = "获取当前监测状态")]
-    Active,
 }
 
 async fn answer(
-    cx: UpdateWithCx<AutoSend<Bot>, Message>,
+    bot: AutoSend<Bot>,
+    message: Message,
     command: Command,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let mut users: Vec<i64> = Vec::new();
+
     match command {
-        Command::Help => cx.answer(Command::descriptions()).await?,
-        _ => cx.answer("无法识别的命令").await?
+        Command::Help => bot.send_message(message.chat.id, Command::descriptions()).await?,
+        Command::Start => {
+            users.push(message.chat.id);
+            let posts = match liuliget::Liuliget::get_page().await {
+                Ok(p) => p,
+                Err(error) => panic!("Problem opening the file: {:?}", error),
+            };
+            for post in &posts {
+                println!("{}", &post.title);
+                println!("{}", &post.img);
+                bot.send_photo(message.chat.id, InputFile::url(Url::parse(&post.img)?)).await?;
+            }
+            bot.send_message(message.chat.id,"finish").await?
+            // // let posts = liuliget::Liuliget::get_page().await;
+            // if let Ok(post) = liuliget::Liuliget::get_page().await {
+            //     bot.send_message(message.chat.id,"success").await?
+            // } else {
+            //     bot.send_message(message.chat.id,"获取异常").await?
+            // }
+        }
     };
 
     Ok(())
@@ -32,27 +49,14 @@ async fn answer(
 #[tokio::main]
 async fn main() {
     teloxide::enable_logging!();
-    log::info!("Starting dices_bot...");
+    log::info!("Starting simple_commands_bot...");
 
     let bot = Bot::from_env().auto_send();
-    let bot_name: String = String::from("alan_dev_dev");
-    let mut liuliget = liuliget::Liuliget::new();
-    liuliget.start();
-    // TODO: https://github.com/teloxide/teloxide/pull/498
+
     bot.set_my_commands(vec![
         teloxide::types::BotCommand::new("help", "查看帮助"),
-        teloxide::types::BotCommand::new("start", "开启定时监测"),
-        teloxide::types::BotCommand::new("stop", "停止定时监测"),
-        teloxide::types::BotCommand::new("refresh", "获取第一页的内容"),
-        teloxide::types::BotCommand::new("active", "获取当前监测状态"),
+        teloxide::types::BotCommand::new("start", "开始使用"),
     ]).send().await.expect("commands set error");
-    teloxide::commands_repl(bot, bot_name, answer).await;
-}
 
-async fn handle_message(
-    cx: UpdateWithCx<AutoSend<Bot>, Message>,
-) {
-    if let Some(ans) = cx.update.text().map(ToOwned::to_owned) {
-        log::info!("re text: {}", ans);
-    }
+    teloxide::repls2::commands_repl(bot, answer, Command::ty()).await;
 }
