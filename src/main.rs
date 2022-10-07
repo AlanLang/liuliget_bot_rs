f537641545969f93410b1207ddc91810d23222f8
Alan <langwdalan@gmail.com>
2022-10-07 16:21:25 +0800

    增加开始订阅的命令 (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,54 @@
-use teloxide::payloads::SendPhotoSetters;
-use teloxide::{prelude2::*};
+use teloxide::{prelude::*};
 use teloxide::types::InputFile;
 use tokio::time;
 use std::error::Error;
 use reqwest::Url;
-mod liuliget;
+use liuliget_bot::post;
+use liuliget_bot::command::{self, Command};
+use teloxide::utils::command::BotCommands;
 
-
-async fn bot_send_post(bot: &AutoSend<Bot>, chat_id: i64, post: &liuliget::Post) -> Result<(), Box<dyn Error + Send + Sync>> {
+async fn bot_send_post(bot: &AutoSend<Bot>, chat_id: ChatId, post: &post::Post) -> Result<(), Box<dyn Error + Send + Sync>> {
     let mut post_message: String = "".to_owned();
     post_message.push_str(&post.title);
     post_message.push_str(&post.description);
     post_message.push_str(&post.post_type.trim());
-    let download_url = match liuliget::Liuliget::get_download(&post.url).await {
+
+    let download_url = match post::get_download(&post.url).await {
         Ok(p) => p,
         Err(error) => panic!("获取下载地址失败: {:?}", error),
     };
     post_message.push_str("\n");
     post_message.push_str(&download_url);
-    let _ = bot.send_photo(chat_id, InputFile::url(Url::parse(&post.img)?))
+    let url = Url::parse(&post.img).unwrap();
+    let photo = InputFile::url(url);
+    log::info!("发送文章给:{}, 标题为:{}", chat_id, post.title);
+    let _ = bot.send_photo(chat_id, photo)
         .caption(post_message)
         .send().await?;
     Ok(())
 }
 
-async fn timer_to_send(bot: AutoSend<Bot>, chat_id: i64) {
+async fn timer_to_send(bot: AutoSend<Bot>, chat_id: ChatId) {
     let mut interval = time::interval(time::Duration::from_secs(5 * 60));
     let mut post_url: String = String::new();
     loop {
         interval.tick().await;
-        let posts = match liuliget::Liuliget::get_page().await {
+        let posts = match post::get_page().await {
             Ok(p) => p,
             Err(error) => panic!("Problem opening the file: {:?}", error),
         };
         let post = posts.first();
+        log::info!("文章标题：{}", post.unwrap().title);
         match post {
             Some(x) => {
                 if post_url != x.url {
                     let _ = bot_send_post(&bot, chat_id, x).await;
-                    post_url =  x.url.to_string();
-                    println!("send success");
+                    post_url =  x.url.to_string(); 
+                } else {
+                    log::info!("文章[{}]已经发送过了，不再发送", x.title);
                 }
             },
-            None    => println!("Cannot divide by 0"),
+            None    => log::info!("Cannot divide by 0"),
         }
 
     }
@@ -50,14 +56,23 @@ async fn timer_to_send(bot: AutoSend<Bot>, chat_id: i64) {
 
 #[tokio::main]
 async fn main() {
-    teloxide::enable_logging!();
-    log::info!("Starting simple_commands_bot...");
-
+    env_logger::init();
     let bot = Bot::from_env().auto_send();
+    // let chats: Vec<ChatId> = Vec::new();
+    teloxide::commands_repl(bot, answer, command::Command::ty()).await;
+}
 
-    teloxide::repls2::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
-        tokio::spawn(timer_to_send(bot,message.chat.id));
-        respond(())
-    })
-    .await;
+async fn answer(
+    bot: AutoSend<Bot>,
+    message: Message,
+    command: Command,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match command {
+        Command::Start => {
+            bot.send_message(message.chat.id, "订阅成功").await?;
+            tokio::spawn(timer_to_send(bot,message.chat.id));
+        }
+    };
+
+    Ok(())
 }
---
+++ src/post.rs
@@ -0,0 +1,49 @@
+use nipper::Document;
+use regex::Regex;
+
+#[derive(Debug)]
+pub struct Post {
+    pub title: String,
+    pub url: String,
+    pub description: String,
+    pub img: String,
+    pub post_type: String,
+}
+
+pub async fn get_page() -> Result<Vec<Post>, Box<dyn std::error::Error>> {
+    let res = reqwest::get("https://www.hacg.cat/wp/").await?.text().await?;
+    let document = Document::from(res.as_str());
+    let mut posts: Vec<Post> = Vec::new();
+    document.select(".status-publish").iter().for_each(|athing| {
+        let title = athing.select(".entry-title a").text();
+        if !title.is_empty(){
+            let url = athing.select(".more-link").attr("href").unwrap_or_default();
+            let description = athing.select(".entry-content p").text().replace(" 继续阅读 →", "");
+            let img = athing.select(".entry-content img").attr("src").unwrap_or_default();
+            let post_type = athing.select(".cat-links").text().replace("发表在 ", "");
+            let post = Post {
+                title: title.to_string(),
+                url: url.to_string(),
+                description: description,
+                img: img.to_string(),
+                post_type: post_type.to_string(),
+            };
+            posts.push(post);
+        }
+    });
+    Ok(posts)
+}
+
+pub async fn get_download(url: &String) -> Result<String, Box<dyn std::error::Error>> {
+    let res = reqwest::get(url).await?.text().await?;
+    let document = Document::from(res.as_str());
+    let dom = document.select(".entry-content").text();
+    let re = Regex::new(r"[0-9a-fA-F]{40,}.*").unwrap();
+    let a:Vec<&str> = re.find_iter(&dom).map(|mat| mat.as_str()).collect();
+    for text in &a {
+        let mut owned_string: String = "magnet:?xt=urn:btih ".to_owned();
+        owned_string.push_str(text);
+        return Ok(owned_string);
+    }
+    Ok(url.to_string())
+}
