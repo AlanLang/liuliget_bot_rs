64b5897caa00ba5d4fbf89c0ed5e6c9f2d272af1
alan <langwdalan@gmail.com>
2023-03-10 19:49:26 +0800

    fix url

--- src/post.rs
+++ src/post.rs
@@ -11,26 +11,38 @@ pub struct Post {
 }
 
 pub async fn get_page() -> Result<Vec<Post>, Box<dyn std::error::Error>> {
-    let res = reqwest::get("https://www.hacg.cat/wp/").await?.text().await?;
+    let res = reqwest::get("https://www.hacg.cat/wp/")
+        .await?
+        .text()
+        .await?;
     let document = Document::from(res.as_str());
     let mut posts: Vec<Post> = Vec::new();
-    document.select(".status-publish").iter().for_each(|athing| {
-        let title = athing.select(".entry-title a").text();
-        if !title.is_empty(){
-            let url = athing.select(".more-link").attr("href").unwrap_or_default();
-            let description = athing.select(".entry-content p").text().replace(" 继续阅读 →", "");
-            let img = athing.select(".entry-content img").attr("src").unwrap_or_default();
-            let post_type = athing.select(".cat-links").text().replace("发表在 ", "");
-            let post = Post {
-                title: title.to_string(),
-                url: url.to_string(),
-                description: description,
-                img: img.to_string(),
-                post_type: post_type.to_string(),
-            };
-            posts.push(post);
-        }
-    });
+    document
+        .select(".status-publish")
+        .iter()
+        .for_each(|athing| {
+            let title = athing.select(".entry-title a").text();
+            if !title.is_empty() {
+                let url = athing.select(".more-link").attr("href").unwrap_or_default();
+                let description = athing
+                    .select(".entry-content p")
+                    .text()
+                    .replace(" 继续阅读 →", "");
+                let img = athing
+                    .select(".entry-content img")
+                    .attr("src")
+                    .unwrap_or_default();
+                let post_type = athing.select(".cat-links").text().replace("发表在 ", "");
+                let post = Post {
+                    title: title.to_string(),
+                    url: url.to_string(),
+                    description: description,
+                    img: img.to_string(),
+                    post_type: post_type.to_string(),
+                };
+                posts.push(post);
+            }
+        });
     Ok(posts)
 }
 
@@ -39,9 +51,9 @@ pub async fn get_download(url: &String) -> Result<String, Box<dyn std::error::Er
     let document = Document::from(res.as_str());
     let dom = document.select(".entry-content").text();
     let re = Regex::new(r"[0-9a-fA-F]{40,}.*").unwrap();
-    let a:Vec<&str> = re.find_iter(&dom).map(|mat| mat.as_str()).collect();
+    let a: Vec<&str> = re.find_iter(&dom).map(|mat| mat.as_str()).collect();
     for text in &a {
-        let mut owned_string: String = "magnet:?xt=urn:btih ".to_owned();
+        let mut owned_string: String = "magnet:?xt=urn:btih:".to_owned();
         owned_string.push_str(text);
         return Ok(owned_string);
     }
